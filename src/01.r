ea4cd3c073b58ef2da1db409f48d2f81be45b40b
Sophia Ruijters <sophia.l.ruijters@hotmail.com>
2023-01-15 21:11:40 +0100

    first commit

---
+++ src/01.r
@@ -0,0 +1,14 @@
+# fix weird working directory behavior
+print(getwd())
+setwd("C:/Users/Gebruiker/Gitrepos/adventofcode") # ugly solution
+print(getwd())
+
+# get data in memory
+input_file <- file("./inputs/01.txt", open="r", blocking = TRUE)
+raw_data <- readLines(input_file)
+close(input_file)
+print(raw_data)
+# write loop which starts new "counter" every blank line
+# make array with cals per elf by adding until blank
+# determine which value is the highest
+# print result
